c50d79211f917d72fb31d2f0e6487acc4959f8a5
Jan Wilhelm <jan.b.h.wilhelm@gmx.de>
2018-06-29 19:38:52 +0200

    Added chat view for the messages of a case

--- resources/views/schueler/case.blade.php
+++ resources/views/schueler/case.blade.php
@@ -2,14 +2,10 @@
 
 @section('content')
 	@exclamoflexsection (["classes" => "bg-color-primary-1 text-white"])
-		<h4>@lang('messages.incidents')</h4>
-		<div class="case-card text-white {{ $case->solved ? "case-solved" : "" }}">
-			<div class="d-sm-flex justify-content-between">
-				<a href="{{ route('incidents.show', ["case" => $case]) }}">
-					<h6 class="font-weight-bold text-white">{{ $case->title }}</h6>
-				</a>
-				<span>Case #{{ $case->id }}</span>
-			</div>
+		<h4>
+			{{ $case->title }}
+		</h4>
+		<div>
 			@if($case->messages()->count() > 0)
 				<span>
 					Update vom {{ $case->messages->first()->updated_at }}
@@ -19,5 +15,32 @@
 				</span>
 			@endif
 		</div>
+		<hr>
+		<div style="overflow: hidden; width: 100%;">
+			<div id="chat-container" class="chat-container p-4 d-flex flex-column" style="overflow-y: auto; width: calc(100% + 18px);">
+				@foreach($case->messages()->orderBy('updated_at', 'asc')->get() as $message)
+					<div class="chat-message p-3 {{ $message->sender->id == auth()->user()->id ? "align-self-end right" : "left" }}">
+						@if($message->sender->id != auth()->user()->id)
+							<a href="" class="text-white mb-2 d-block">{{ $message->sender->fullName() }}</a>
+						@endif
+						<span class="chat-text">
+							{{ $message->body }}
+						</span>
+						<span class="chat-time">
+							{{ $message->updated_at->format("d.m.Y G:i") }}
+						</span>
+					</div>
+				@endforeach
+			</div>
+		</div>
 	@endexclamoflexsection
 @endsection
+
+@push('scripts')
+	<script type="text/javascript">
+		$('document').ready(function() {
+			var cont = document.getElementById("chat-container");
+			cont.scrollTop = cont.scrollHeight;
+		})
+	</script>
+@endpush
