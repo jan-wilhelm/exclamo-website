589581268da615622ada567550a87c462da8031e
Jan Wilhelm <jan.b.h.wilhelm@gmx.de>
2018-07-02 20:21:33 +0200

    Added SendApiTokenMiddleware to make the Api Token available to the javascript ajax requests

---
+++ app/http/Middleware/SendApiTokenMiddleware.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
+
+class SendApiTokenMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Auth::check()) {
+            $token = Auth::user()->api_token;
+            $cookie = Cookie::make(
+                'API-TOKEN',        // name
+                $token,             // value
+                time() + 60 * 24,   // expire
+                "/",                // path
+                "",                 // domain
+                false,              // secure
+                false               // http-only
+            );
+            Cookie::queue($cookie);
+        }
+
+        return $next($request);
+    }
+}
