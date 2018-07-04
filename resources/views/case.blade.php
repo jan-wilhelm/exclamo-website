3e83c1d3b1f871abe083ace979fd196f33d008b8
Jan Wilhelm <jan.b.h.wilhelm@gmx.de>
2018-07-04 14:20:36 +0200

    Styling issues

--- resources/views/case.blade.php
+++ resources/views/case.blade.php
@@ -2,15 +2,17 @@
 
 @section('content')
 	@exclamoflexsection
-		<h4>
-			{{ $case->title }}
-		</h4>
-		<span class="font-italic">
-			Mentored by {{ implode(", ", $case->mentors->map(function ($mentor) {
-				$mentor->full_name = $mentor->first_name . " " . $mentor->last_name;
-				return $mentor;
-			})->pluck("full_name")->toArray()) }}
-		</span>
+		<div>
+			<h4 class="d-md-inline-block">
+				{{ $case->title }}
+			</h4>
+			<span class="d-inline-block font-italic">
+				Mentored by {{ implode(", ", $case->mentors->map(function ($mentor) {
+					$mentor->full_name = $mentor->first_name . " " . $mentor->last_name;
+					return $mentor;
+				})->pluck("full_name")->toArray()) }}
+			</span>
+		</div>
 		<hr>
 
 		<chat-messages-container :messages="{{ json_encode($messages) }}"> </chat-messages-container>
