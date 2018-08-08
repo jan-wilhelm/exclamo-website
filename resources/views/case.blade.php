b7448dba005bafceded34345255be2870e0e941c
Jan Wilhelm <jan.b.h.wilhelm@gmx.de>
2018-08-08 14:33:55 +0200

    Use helper functions

--- resources/views/case.blade.php
+++ resources/views/case.blade.php
@@ -8,7 +8,7 @@
 			</h4>
 			<span class="d-md-inline-block font-italic ml-md-4">
 				Mentored by {{ implode(", ", $case->mentors->map(function ($mentor) {
-					$mentor->full_name = $mentor->first_name . " " . $mentor->last_name;
+					$mentor->full_name = $mentor->fullName();
 					return $mentor;
 				})->pluck("full_name")->toArray()) }}
 			</span>
