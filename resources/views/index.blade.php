9dec710953736a1baa24026598bfff8a7b5ef720
Jan Wilhelm <jan.b.h.wilhelm@gmx.de>
2018-08-14 23:13:18 +0200

    Basic landing page layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,30 +10,90 @@
 @endsection
 
 @section("body")
-	<section class="landing-section bg-color-primary-0">
+	<section class="landing-section">
 		<div class="col-12 brand justify-content-center d-flex">
-			<img src="http://localhost/img/logo_small.png">
+			<img src="{{ asset('img/logo_with_text.png') }}">
 		</div>
-		<div class="container">
-			<div class="row">
-				<h2 class="col-sm-3 promo-question">
-					Was?
-				</h2>
-				<div class="col-sm-9 promo-text">
-					exclamo hilft Schülern, die Opfer von Mobbing sind, über ihre Probleme zu sprechen und sie zu lösen. Schüler einer exclamo-Schule bekommen einen Account für die App exclamo, über die sie sich auch anonym an ihre Lehrer und externe Mobbing-Experten wenden können. Zudem gibt es hilfreiche Tipps, die im Umgang mit Mobbing helfen und den Schüler unterstützen, mit Vorfällen fertig zu werden. Damit verbindet exclamo eine niedrige Hemmschwelle mit einer einfachen Lösung von Vorfällen, durch Lehrer, die direkt in der Schule Maßnahmen ergreifen können. Mit dem Format einer App und Web-App ist exclamo in dem Medium, das Schüler am meisten Nutzen – dem Smartphone - und bieten so für den Betroffenen eine native Nutzung.
+		<div class="wrapper">
+			<div class="container-fluid">
+				<div class="row">
+					<h2 class="col-sm-3 promo-question">
+						Was?
+					</h2>
+					<div class="col-sm-9 promo-text">
+						exclamo hilft Schülern, die Opfer von Mobbing sind, über ihre Probleme zu sprechen und sie zu lösen. Schüler einer exclamo-Schule bekommen einen Account für die App exclamo, über die sie sich auch anonym an ihre Lehrer und externe Mobbing-Experten wenden können. Zudem gibt es hilfreiche Tipps, die im Umgang mit Mobbing helfen und den Schüler unterstützen, mit Vorfällen fertig zu werden. Damit verbindet exclamo eine niedrige Hemmschwelle mit einer einfachen Lösung von Vorfällen, durch Lehrer, die direkt in der Schule Maßnahmen ergreifen können. Mit dem Format einer App und Web-App ist exclamo in dem Medium, das Schüler am meisten Nutzen – dem Smartphone - und bieten so für den Betroffenen eine native Nutzung.
+					</div>
 				</div>
 			</div>
 		</div>
 	</section>
 
-	<section class="landing-section bg-color-secondary-1-2">
-		<div class="container">
-			<div class="row">
-				<div class="col-3">
-					Was?
+	<section class="landing-section">
+		<div class="wrapper">
+			<div class="container-fluid">
+				<div class="row">
+					<h2 class="col-sm-3 promo-question">
+						Wer?
+					</h2>
+					<div class="col-sm-9 promo-text">
+						Wir, das Team, sind Berliner Schüler und haben uns überlegt, wie man Mobbing am besten bekämpfen kann. Da das Handy einer der Alltagsgestände schlechthin ist, schien außer Frage, dass eine App das beste Format ist.
+					</div>
 				</div>
-				<div class="col-9">
-					exclamo hilft Schülern, die Opfer von Mobbing sind, über ihre Probleme zu sprechen und sie zu lösen. Schüler einer exclamo-Schule bekommen einen Account für die App exclamo, über die sie sich auch anonym an ihre Lehrer und externe Mobbing-Experten wenden können. Zudem gibt es hilfreiche Tipps, die im Umgang mit Mobbing helfen und den Schüler unterstützen, mit Vorfällen fertig zu werden. Damit verbindet exclamo eine niedrige Hemmschwelle mit einer einfachen Lösung von Vorfällen, durch Lehrer, die direkt in der Schule Maßnahmen ergreifen können. Mit dem Format einer App und Web-App ist exclamo in dem Medium, das Schüler am meisten Nutzen – dem Smartphone - und bieten so für den Betroffenen eine native Nutzung.
+			</div>
+		</div>
+	</section>
+	<section class="landing-section bg-color-secondary-1-2">
+		<div class="wrapper">
+			<div class="container-fluid">
+				<div class="row">
+					<h2 class="col-sm-3 promo-question">
+						Wer?
+					</h2>
+					<div class="col-sm-9 promo-text">
+						Wir, das Team, sind Berliner Schüler und haben uns überlegt, wie man Mobbing am besten bekämpfen kann. Da das Handy einer der Alltagsgestände schlechthin ist, schien außer Frage, dass eine App das beste Format ist.
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="landing-section bg-color-secondary-1-2">
+		<div class="wrapper">
+			<div class="container-fluid">
+				<div class="row">
+					<h2 class="col-sm-3 promo-question">
+						Wer?
+					</h2>
+					<div class="col-sm-9 promo-text">
+						Wir, das Team, sind Berliner Schüler und haben uns überlegt, wie man Mobbing am besten bekämpfen kann. Da das Handy einer der Alltagsgestände schlechthin ist, schien außer Frage, dass eine App das beste Format ist.
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="landing-section bg-color-secondary-1-2">
+		<div class="wrapper">
+			<div class="container-fluid">
+				<div class="row">
+					<h2 class="col-sm-3 promo-question">
+						Wer?
+					</h2>
+					<div class="col-sm-9 promo-text">
+						Wir, das Team, sind Berliner Schüler und haben uns überlegt, wie man Mobbing am besten bekämpfen kann. Da das Handy einer der Alltagsgestände schlechthin ist, schien außer Frage, dass eine App das beste Format ist.
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="landing-section bg-color-secondary-1-2">
+		<div class="wrapper">
+			<div class="container-fluid">
+				<div class="row">
+					<h2 class="col-sm-3 promo-question">
+						Wer?
+					</h2>
+					<div class="col-sm-9 promo-text">
+						Wir, das Team, sind Berliner Schüler und haben uns überlegt, wie man Mobbing am besten bekämpfen kann. Da das Handy einer der Alltagsgestände schlechthin ist, schien außer Frage, dass eine App das beste Format ist.
+					</div>
 				</div>
 			</div>
 		</div>
