4bcd1e131123863644d2b0ac5dd7171dafb698c2
Jan Wilhelm <jan.b.h.wilhelm@gmx.de>
2018-08-21 18:01:20 +0200

    Cases redesign

--- resources/views/components/cases/casesview.blade.php
+++ resources/views/components/cases/casesview.blade.php
@@ -1,20 +1,21 @@
 <div class="row mb-4 p-3">
 @foreach($cases as $case)
-	<div class="case-card shadow bg-white col-lg-3 col-md-5 mr-lg-5 mr-md-3 mb-5 {{ $case->solved ? "case-solved" : "" }}">
-		<div class="d-sm-flex justify-content-between">
+	<div class="d-flex flex-column case-card shadow bg-white col-lg-3 col-md-5 mr-lg-5 mr-md-3 mb-5 {{ $case->solved ? "case-solved" : "" }}">
+		<div class="mb-auto pb-2">
 			<a href="{{ route('incidents.show', ["case" => $case]) }}">
 				<h5 class="font-weight-bold">{{ $case->title }}</h5>
 			</a>
-		</div>
 
-		<span>
-			@if($case->messages->count() > 0)
-				{{ str_limit($case->messages->first()->body, 140) }}
-			@endif
-		</span>
-		<div class="row d-none">
-			<a href="{{ route('incidents.show', ["case" => $case]) }}">
-				<h6 class="small mt-2">Zum Fall</h6>
+			<span>
+				@if($case->messages->count() > 0)
+					{{ str_limit($case->messages->first()->body, 140) }}
+				@endif
+			</span>
+		</div>
+		<hr>
+		<div class="d-inline-flex align-items-end justify-content-between">
+			<a class="small" href="{{ route('incidents.show', ["case" => $case]) }}">
+				Zum Fall
 			</a>
 			<span class="small">
 				@if($case->messages->count() > 0)
