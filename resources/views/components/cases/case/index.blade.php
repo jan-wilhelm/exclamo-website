ae6c618ae4b84a9a8aa7580c769e0016f0ecf768
Jan Wilhelm <jan.b.h.wilhelm@gmx.de>
2018-08-21 21:53:36 +0200

    Case styling

--- resources/views/components/cases/case/index.blade.php
+++ resources/views/components/cases/case/index.blade.php
@@ -1,8 +1,8 @@
 <div>
-	<h4 class="d-md-inline-block">
+	<h4 class="d-md-inline-block font-weight-bold">
 		{{ $title }}
 	</h4>
-	<span class="d-md-inline-block font-italic ml-md-4">
+	<span class="d-md-inline-block ml-md-4">
 		{{ $description }}
 	</span>
 	@isset ($header)
