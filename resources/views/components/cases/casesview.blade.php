8d5961b55b682b3b70d7f421925b53f0617d11bc
Jan Wilhelm <jan.b.h.wilhelm@gmx.de>
2018-08-21 18:26:01 +0200

    Change report case button

--- resources/views/components/cases/casesview.blade.php
+++ resources/views/components/cases/casesview.blade.php
@@ -1,6 +1,6 @@
 <div class="row mb-4 p-3">
-@foreach($cases as $case)
-	<div class="d-flex flex-column case-card shadow bg-white col-lg-3 col-md-5 mr-lg-5 mr-md-3 mb-5 {{ $case->solved ? "case-solved" : "" }}">
+	@foreach($cases as $case)
+	<div class="d-flex flex-column case-card shadow col-lg-3 col-md-5 mr-lg-5 mr-md-3 mb-5 {{ $case->solved ? "case-solved" : "" }}">
 		<div class="mb-auto pb-2">
 			<a href="{{ route('incidents.show', ["case" => $case]) }}">
 				<h5 class="font-weight-bold">{{ $case->title }}</h5>
@@ -16,7 +16,7 @@
 		<div class="d-flex flex-column">
 			<div class="w-100 d-flex mb-3">
 				<span class="tag">
-					<i class="fas fa-user-friends"></i>
+					<i class="fas fa-user-friends mr-1"></i>
 					{{ $case->mentors->count() }} Mentors
 				</span>
 			</div>
@@ -32,5 +32,6 @@
 			</div>
 		</div>
 	</div>
-@endforeach
+	@endforeach
+	{{ $slot }}
 </div>
--- resources/views/schueler/cases.blade.php
+++ resources/views/schueler/cases.blade.php
@@ -21,13 +21,14 @@
 		</h3>
 		
 		@casesview (["cases" => $cases])
+			<div class="d-flex flex-column case-card create-case shadow col-lg-3 col-md-5 py-5 align-items-center justify-content-center">
+				<a href="{{ route('incidents.report') }}" class="text-center">
+					<h5 class="font-weight-bold">
+						@lang('messages.createcase')
+					</h5>
+					<i class="fas fa-plus fa-4x color-primary-1"></i>
+				</a>
+			</div>
 		@endcasesview
-
-		<div class="create-case bg-color-primary-2 d-flex align-content-center transition">
-			<a class="m-auto text-white" href="{{ route('incidents.report') }}">
-				<i class="fas fa-plus mr-sm-1"></i>
-				@lang('messages.createcase')
-			</a>
-		</div>
 	@endexclamoflexsection
 @endsection
