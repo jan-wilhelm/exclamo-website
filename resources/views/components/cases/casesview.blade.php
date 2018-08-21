45e8c957e03ef94d2d680bbdf8fb5326b0911801
Jan Wilhelm <jan.b.h.wilhelm@gmx.de>
2018-08-21 18:11:02 +0200

    Mentor tags

--- resources/views/components/cases/casesview.blade.php
+++ resources/views/components/cases/casesview.blade.php
@@ -13,15 +13,23 @@
 			</span>
 		</div>
 		<hr>
-		<div class="d-inline-flex align-items-end justify-content-between">
-			<a class="small" href="{{ route('incidents.show', ["case" => $case]) }}">
-				Zum Fall
-			</a>
-			<span class="small">
-				@if($case->messages->count() > 0)
-					{{ $case->messages->first()->updated_at->diffForHumans() }}
-				@endif
-			</span>
+		<div class="d-flex flex-column">
+			<div class="w-100 d-flex mb-3">
+				<span class="tag">
+					<i class="fas fa-user-friends"></i>
+					{{ $case->mentors->count() }} Mentors
+				</span>
+			</div>
+			<div class="w-100 d-flex align-items-end justify-content-between">
+				<a class="small" href="{{ route('incidents.show', ["case" => $case]) }}">
+					Zum Fall
+				</a>
+				<span class="small">
+					@if($case->messages->count() > 0)
+						{{ $case->messages->first()->updated_at->diffForHumans() }}
+					@endif
+				</span>
+			</div>
 		</div>
 	</div>
 @endforeach
