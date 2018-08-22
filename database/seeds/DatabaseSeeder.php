25a21910096d57abfc740a06281217552334afa3
Jan Wilhelm <jan.b.h.wilhelm@gmx.de>
2018-08-22 21:48:24 +0200

    Only choose tutors who are offering to mentor

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -53,7 +53,7 @@ class DatabaseSeeder extends Seeder
 
 
         $students = App\User::role('schueler')->get();
-        $mentors = App\User::role(['lehrer', 'schulleiter'])->get();
+        $mentors = App\User::role(['lehrer', 'schulleiter'])->mentoring()->get();
 
         foreach ($students as $student) {
 
