8e55e040461aa1c7cac7c6b15f697f1dcecf7f6a
Jan Wilhelm <jan.b.h.wilhelm@gmx.de>
2018-09-12 22:09:49 +0200

    Updated favicons

--- resources/views/layouts/html.blade.php
+++ resources/views/layouts/html.blade.php
@@ -5,17 +5,15 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-
-
         <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/favicon/apple-touch-icon.png') }}">
         <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/favicon/favicon-32x32.png') }}">
         <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicon/favicon-16x16.png') }}">
         <link rel="manifest" href="{{ asset('img/favicon/site.webmanifest') }}">
-        <link rel="mask-icon" href="{{ asset('img/favicon/safari-pinned-tab.svg') }}" color="#5bbad5">
-        <meta name="msapplication-TileColor" content="#da532c">
-        <meta name="theme-color" content="#ffffff">
-
-
+        <link rel="mask-icon" href="{{ asset('img/favicon/safari-pinned-tab.svg') }}" color="#009489">
+        <meta name="apple-mobile-web-app-title" content="exclamo">
+        <meta name="application-name" content="exclamo">
+        <meta name="msapplication-TileColor" content="#009489">
+        <meta name="theme-color" content="#009489">
 
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
