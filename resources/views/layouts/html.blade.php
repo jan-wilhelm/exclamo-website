81e78d9d7a0706884d22e8ebec1fe6c35d5b4d5f
Jan Wilhelm <jan.b.h.wilhelm@gmx.de>
2018-09-12 22:22:07 +0200

    Update links to favicons

--- resources/views/layouts/html.blade.php
+++ resources/views/layouts/html.blade.php
@@ -5,11 +5,11 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/favicon/apple-touch-icon.png') }}">
-        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/favicon/favicon-32x32.png') }}">
-        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicon/favicon-16x16.png') }}">
-        <link rel="manifest" href="{{ asset('img/favicon/site.webmanifest') }}">
-        <link rel="mask-icon" href="{{ asset('img/favicon/safari-pinned-tab.svg') }}" color="#009489">
+        <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+        <link rel="manifest" href="site.webmanifest">
+        <link rel="mask-icon" href="safari-pinned-tab.svg" color="#009489">
         <meta name="apple-mobile-web-app-title" content="exclamo">
         <meta name="application-name" content="exclamo">
         <meta name="msapplication-TileColor" content="#009489">
