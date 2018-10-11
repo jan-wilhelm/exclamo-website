f20751f1745230de9c2dbfa9af732cdbb32d532e
Jan Wilhelm <jan.b.h.wilhelm@gmx.de>
2018-10-11 17:48:33 +0200

    Fixed spelling mistakes

--- resources/views/landing_page/de/schools.blade.php
+++ resources/views/landing_page/de/schools.blade.php
@@ -1,5 +1,5 @@
 <p>
-	Sie sind in der Schulleitung einer Schule oder in einem Schulträger tätig und ihnen gefällt das Konzept hinter exclamo?
+	Sie sind in der Schulleitung einer Schule oder in einem Schulträger tätig und Ihnen gefällt das Konzept hinter exclamo?
 </p>
 <p>
 	exclamo-Schulen bekommen von uns einen Administrator-Account und einen definiertes Kontingent an Lehrer- und Schüler-Accounts. Die Schüler haben dann die Möglichkeit, über die Website oder Android-/iOS-App direkt bestimmten Lehrern oder externen Mobbing-Experten unter psychotherapeutischer Aufsicht von ihren Mobbingfällen zu berichten. Lehrer und Schüler erhalten Zugang zu exklusiven Anti-Mobbing-Materialien, die ihnen im Umgang mit Mobbing im Alltag helfen. Die Materialien für Lehrer beinhalten Anweisungen, wie sie mit Problemen umgehen können. Die Schüler können zudem per Knopfdruck bei Sorgentelefonen wie der Nummer-gegen-Kummer anrufen, um seelische Unterstützung und Mut zu bekommen.
