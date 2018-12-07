3979b200ea4db21bd92ffeb877ff611ebb669496
Jan Wilhelm <jan.b.h.wilhelm@gmx.de>
2018-12-07 21:34:12 +0100

    edited slogan

--- resources/views/landing_page/de/slogan.blade.php
+++ resources/views/landing_page/de/slogan.blade.php
@@ -1,1 +1,1 @@
-Mit einem einzigartigen Ansatz helfen wir Schulen, Mobbing zu bekämpfen: Ihre Schüler wenden sich per exclamo an ausgewählte Lehrer und professionelle Mobbing-Experten und erhalten so schnelle und anonyme Hilfe!
+Mit einem einzigartigen Ansatz helfen wir Schulen, Mobbing zu bekämpfen. Mobbing melden - Lösung finden: Einfach. Schnell. Anonym.
